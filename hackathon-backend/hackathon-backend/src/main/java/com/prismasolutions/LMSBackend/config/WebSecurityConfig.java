515d7236795e28999ba650874686873156b39198
Atesz23 <60443255+Atesz23@users.noreply.github.com>
2023-03-19 01:16:22 +0200

    wqed

--- hackathon-backend/hackathon-backend/src/main/java/com/prismasolutions/LMSBackend/config/WebSecurityConfig.java
+++ hackathon-backend/hackathon-backend/src/main/java/com/prismasolutions/LMSBackend/config/WebSecurityConfig.java
@@ -51,15 +51,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
         configuration.setAllowedMethods(Arrays.asList("HEAD",
                 "GET", "POST", "PUT", "DELETE", "PATCH"));
         // setAllowCredentials(true) is important, otherwise:
         // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
-        configuration.setAllowCredentials(true);
+//        configuration.setAllowCredentials(true);
         // setAllowedHeaders is important! Without it, OPTIONS preflight request
         // will fail with 403 Invalid CORS request
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type","Basic"));
+        configuration.setAllowedHeaders(Arrays.asList( "Accept", "Authorization", "Cache-Control", "Content-Type","Basic"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
